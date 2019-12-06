90386a03614fc3cff483feefd21edfb8f3d133a3
Asif Kamal <asif_kamal@hotmail.com>
2019-12-06 21:30:15 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -7,8 +7,8 @@ BASE_HOA = {
 
 def add_character(show, name)
 
-  [:chipmunks] << "Dave"
-  [:third_earthers] << "Snarf"
-  [:jetsons] << "Astro"
-
+ BASE_HOA = {[:chipmunks] << "Dave"
+             [:third_earthers] << "Snarf"
+             [:jetsons] << "Astro"
+ }
 end
